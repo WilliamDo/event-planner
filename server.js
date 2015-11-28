642d3abe98b38ffee763ca4ee4209c28b3911426
William Do <william@ultimaspin.com>
2015-11-28 19:09:06 +0000

    update start-up log message

--- server.js
+++ server.js
@@ -164,7 +164,5 @@ app.get('/friends', function(req, res) {
 });
 
 var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Listening at http://%s:%s', host, port);
+  console.log('Listening on port %s', server.address().port);
 });
